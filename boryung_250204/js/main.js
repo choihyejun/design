666ed5ed076389371def9f03416f48390e6912c2
choihyejun <d0_0@naver.com>
2025-02-10 18:01:51 +0900

    250210보령

--- boryung_250204/js/main.js
+++ boryung_250204/js/main.js
@@ -35,22 +35,22 @@ $(document).ready(function(){
     })
     /**************************** visual 팝업 (종료) ************************************/
     
-    const product_swiper = new Swiper('.product .list .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-        speed: 5000,
-        breakpoints: {
-            640: {    /* 640px 이상일때 적용 */
-                spaceBetween: 24,
-            },
-        },
-        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        autoplay: {  /* 팝업 자동 실행 */
-            delay: 1,
-            disableOnInteraction: false,
-        },
-    });
+     const product_swiper = new Swiper('.product .list .swiper', { /* 팝업을 감싼는 요소의 class명 */
+         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+         speed: 5000,
+         breakpoints: {
+             640: {    /* 640px 이상일때 적용 */
+                 spaceBetween: 24,
+             },
+         },
+         //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+         autoplay: {  /* 팝업 자동 실행 */
+             delay: 1,
+             disableOnInteraction: false,
+         },
+     });
 
     $('.product .tit .ctrl_btn .stop').on('click', function(){
         //console.log('정지버튼 클릭')
