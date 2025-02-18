75350a685569d9ac75542caff15ef0943e7be4c2
choihyejun <d0_0@naver.com>
2025-02-18 18:02:06 +0900

    20250218_체리부로

--- boryung_250204/js/main.js
+++ boryung_250204/js/main.js
@@ -35,6 +35,20 @@ $(document).ready(function(){
     })
     /**************************** visual 팝업 (종료) ************************************/
     
+    /************************* 탭 기능 (시작)  **************************/
+    $('.find .tab_list ul li').on('click', function(){
+        $('.find .tab_list ul li').removeClass('active')
+        $(this).addClass('active')
+        $('.find .tab_list ul li button').attr('title', '')
+        $(this).find('button').attr('title', '선택됨')
+        tab_name = $(this).attr('data-tab')
+        console.log(tab_name)
+        $('.find .tab_content .tab_panel').removeClass('active')
+        $('.find .tab_content').find('[data-tab="'+tab_name+'"]').addClass('active')
+    })
+    /************************* 탭 기능 (종료)  **************************/
+
+    /**************************** product 팝업 (시작) ************************************/
      const product_swiper = new Swiper('.product .list .swiper', { /* 팝업을 감싼는 요소의 class명 */
          slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
          spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
@@ -65,4 +79,65 @@ $(document).ready(function(){
         $('.product .tit .ctrl_btn .stop').show()
     })
  
+    /**************************** product 팝업 (종료) ************************************/
+    
+    /**************************** media 팝업 (시작) ************************************/ 
+    const media_swiper = new Swiper('.media .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            1024: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 20,
+            },
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 20,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+    });
+    /**************************** product 팝업 (종료) ************************************/
+
+
+    /**************************** family 열고 닫기 (시작) **********************************
+     * footer .f_site .family button.site_open 을 클릭했을때 
+     * footer .f_site .family 에 open 클래스 추가
+     * footer .f_site .family button.site_close 를 클릭했을때
+     * footer .f_site .family 에 open 클래스 삭제
+     * 
+     * footer .f_site .brand button.site_open 을 클릭했을때 
+     * footer .f_site .brand 에 open 클래스 추가
+     * footer .f_site .brand button.site_close 를 클릭했을때
+     * footer .f_site .brand 에 open 클래스 삭제
+     * **/
+
+    $('footer .f_site .family button.site_open').on('click', function(){
+		$('footer .f_site .family').addClass('open');
+	});
+    $('footer .f_site .family button.site_close').on('click', function(){
+		$('footer .f_site .family').removeClass('open');
+	});
+    $('footer .f_site .brand button.site_open').on('click', function(){
+		$('footer .f_site .brand').addClass('open');
+	});
+    $('footer .f_site .brand button.site_close').on('click', function(){
+		$('footer .f_site .brand').removeClass('open');
+	});
+    
+    /**************************** family 열고 닫기 (종료) ************************************/
 })//$(document).ready
